271ad0d0824e4210550e4b05c2a5fde40dfecf9d
sudhanshu shukla <161225935+sudhanshu112233shukla@users.noreply.github.com>
2025-09-20 11:27:56 +0100

    Delete lib directory
    updates .

--- lib/api/signup_branch.ts
+++
@@ -1,9 +0,0 @@
-import { sendEmailToAdmin } from "../utils/email";
-import { supabase } from "../utils/supabase";
-
-export async function registerBranchRequest(data: any) {
-  const insertData = { ...data, status: "pending" };
-  const { error } = await supabase.from("branches").insert(insertData);
-  if (error) throw new Error(error.message);
-  await sendEmailToAdmin(data);
-}
